f821e5dcd40f91a2b96ad80e2665e10e367df581
HytonightYX <whosiyuan@qq.com>
2020-05-25 20:31:37 +0800

    feat: login UI & styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,75 @@
-import React, { Component } from "react";
+import React from "react";
+import { Form, Input, Button } from "antd";
+import { UserOutlined, LockOutlined, MobileOutlined, ContainerOutlined } from "@ant-design/icons";
 
-class App extends Component {
+import "./style.less";
+
+const LOGIN_TYPE = {
+  PWD: 1,
+  SMS: 2,
+  CRL: 3,
+};
+
+class Login extends React.Component {
+  state = {
+    type: LOGIN_TYPE.PWD,
+  };
+
+  onFinish = (values) => {
+    console.log(values);
+  };
+
   render() {
-    return <div>Auth Login</div>;
+    const { type } = this.state;
+    return (
+      <div className='form-warp'>
+        {type === LOGIN_TYPE.PWD && (
+          <Form className='login-form' onFinish={this.onFinish}>
+            <Form.Item name='username' rules={[{ required: true, message: "请输入用户名" }]}>
+              <Input
+                prefix={<UserOutlined className='site-form-item-icon' />}
+                placeholder='用户名'
+              />
+            </Form.Item>
+            <Form.Item name='password' rules={[{ required: true, message: "请输入密码" }]}>
+              <Input
+                prefix={<LockOutlined className='site-form-item-icon' />}
+                type='password'
+                placeholder='密码'
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type='primary' htmlType='submit' className='login-form-button'>
+                登陆
+              </Button>
+            </Form.Item>
+          </Form>
+        )}
+
+        <div className='more'>
+          <div>
+            <MobileOutlined style={{ fontSize: "26px" }} />
+            <span>手机验证码</span>
+          </div>
+
+          <div>
+            <ContainerOutlined style={{ fontSize: "26px" }} />
+            <span>数字证书</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='g'>
+        <Login />
+      </div>
+    );
   }
 }
 
