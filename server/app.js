76640683864c1c824cf60d7f34d0801e10535af4
HytonightYX <whosiyuan@qq.com>
2020-06-02 22:16:45 +0800

    feat: connect to db

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const compression = require('compression');
-
 const db = require('./db');
+
 const app = express();
 const port = 8000;
 
@@ -11,23 +11,10 @@ app.use(compression());
 app.use(cors());
 app.use(bodyParser.json({ limit: '10mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-app.use(express.static(__dirname + '/'));
+app.use(express.static(`${__dirname}/`));
 
-/**
- * 调用 DB procedure
- */
-function procedure(sql, params, res, cb) {
-  db.procedureSQL(sql, JSON.stringify(params), (err, ret) => {
-    if (err) {
-      res.status(500).json({ code: -1, msg: '提交请求失败，请联系管理员！', data: null });
-    } else {
-      cb(ret);
-    }
-  });
-}
-
-app.get('*.js', function (req, res, next) {
-  req.url = req.url + '.gz';
+app.get('*.js', (req, res, next) => {
+  req.url += '.gz';
   res.set('Content-Encoding', 'gzip');
   next();
 });
@@ -35,20 +22,16 @@ app.get('*.js', function (req, res, next) {
 /**
  * 测试接口
  */
-app.get('/test', async function (req, res) {
+app.get('/test', async (req, res) => {
   res.status(200).json({ code: 200, data: {}, msg: '接口测试成功' });
 });
 
 /**
  * 登录
 */
-app.post('/login', async function (req, res) {
-  const sql = `CALL PROC_USER_LOGIN(?)`;
-  const params = req.body;
-  console.log(params);
-  procedure(sql, params, res, (r) => {
-    res.status(200).json({ code: 200, data: r[0], msg: '获取到用户信息' });
-  });
+app.post('/login', async (req, res) => {
+  const data = await db.memberList();
+  res.status(200).json(data);
 });
 
 app.listen(port, () => console.log(`> Running on localhost:${port}`));
--- server/db.js
+++ server/db.js
@@ -1,11 +1,98 @@
 const conf = {
   host: '101.37.14.191',
-  port: 3306,
-  database: 'library',
-  user: 'libadmin',
-  password: require('./secret').mysql_pwd,
-  multipleStatements: true,
-  secret: 'lorem',
+  user: 'ivcuser',
+  database: 'ivc',
+  password: 'ivcpwd',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 };
 
-module.exports = conf;
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool(conf);
+
+async function findUser(username) {
+  const result = await pool.query('SELECT * FROM user WHERE username = ?', [username]);
+  if (result[0].length < 1) {
+    throw new Error('用户未找到');
+  }
+  return result[0][0];
+}
+
+async function findMemberAll() {
+  const result = await pool.query('SELECT * FROM member');
+  return result[0] || [];
+}
+
+const userLogin = async (username, password) => {
+  const userInDB = await findUser(username);
+  if (userInDB.password === password) {
+    return userInDB;
+  }
+  throw new Error('密码错误');
+};
+
+/**
+ * 获取所有成员信息
+ */
+const memberList = async () => await findMemberAll();
+
+/**
+ * 更新用户信息
+ * @param params 需更新的列表
+ */
+const memberEdit = async (params) => {
+  const setSQL = Object.keys(params).map((item) => `${item}="${params[item]}"`);
+  const sql = `UPDATE member SET ${setSQL} WHERE id=${params.id}`;
+  await pool.query(sql);
+};
+
+/**
+ * 获取所有信息缩略信息
+ * 用于首页展示和管理端列表展示（去掉多余的文章详情）
+ */
+const postListSimple = async () => {
+  const [rows] = await pool.query('SELECT id, title, cover, create_time, status FROM post WHERE del IS NULL ORDER BY create_time DESC');
+  return rows || [];
+};
+
+/**
+ * 添加文章
+ */
+const postCreate = async (params) => {
+  const time = dayjs(Date.now()).format('YYYYMMDDhhmmss');
+  const [rows, fields] = await pool.query(`
+		INSERT INTO post (cover, title, html, raw, status, create_time) 
+		VALUES (?, ?, ?, ?, ?, ?)
+	`, [params.cover, params.title, params.html, params.raw, 0, time]);
+  return rows || [];
+};
+
+/**
+ * 删除文章
+ */
+const postDelete = async (params) => {
+  await pool.query(`UPDATE post SET del = 1 WHERE id=${params.id}`);
+};
+
+/**
+ * 获取完整文章
+ */
+const postGetFull = async (params) => {
+  const [rows] = await pool.query(`SELECT id, title, cover, raw, html, create_time FROM post WHERE id = ${params.id} AND del IS NULL`);
+  if (rows[0]) {
+    return rows;
+  }
+  throw new Error('找不到该文档');
+};
+
+module.exports = {
+  userLogin,
+  memberList,
+  memberEdit,
+  postListSimple,
+  postCreate,
+  postDelete,
+  postGetFull,
+};
