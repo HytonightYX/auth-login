5f0f7905b0e7860fb63d60923b3b557e378c2a21
HytonightYX <whosiyuan@qq.com>
2020-06-02 22:15:47 +0800

    style: update eslint config

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": [
-        "airbnb-base"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 11
-    },
-    "rules": {
-    }
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'no-console': 0,
+  },
 };
